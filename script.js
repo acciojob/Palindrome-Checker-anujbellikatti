0e23812f188ade415eaa29ef3b508bcecff77222
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-10-19 23:57:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,13 @@
 
 function palindrome(str){
 
+	let  x = str.split("");
+	for(let i=0;i<x.length;i++){
+		if(x[i]===x[x.length-i]){
+			return true;
+		}else{
+			return false;
+		}
+	}
 }
 module.exports = palindrome
