d43d0f8f827546a5edd4a0ac5a31df9f07084de1
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-10-20 00:08:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 // complete the given function
 
-function palindrome(str){
+// function palindrome(str){
 
-	let  xx = str.split(" ").toString();
-	let x=xx.split("");
-	let c=0;
-	for(let i=0;i<x.length/2;i++){
-		if(x[i]===x[x.length-i]){
+// 	let  xx = str.split(" ").toString();
+// 	let x=xx.split("");
+// 	let c=0;
+// 	for(let i=0;i<x.length/2;i++){
+// 		if(x[i]===x[x.length-i]){
 			
-		}else{
-			c++;
-		}
+// 		}else{
+// 			c++;
+// 		}
+// 	}
+// 	if(c>0){
+// 		return false;
+// 	}else{
+// 		return true;
+// 	}
+// }
+// module.exports = palindrome
+
+function palindrome(str){
+ let ch="" ;
+	str=str.toLowerCase();
+	for (let i = (str.length-1); i >=0 ; i--) {
+		ch+=str[i];
+	
 	}
-	if(c>0){
-		return false;
-	}else{
+	if(ch==str){
 		return true;
+	}else{
+		return false;
 	}
 }
 module.exports = palindrome
